0943b5ef63df811a62004a09220fbde525c8fd61
Madinku <MadinkuFinny@gmail.com>
2023-10-18 10:44:18 +0200

    Adding Browser router on index.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,18 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import useLocation from "react-router-dom"
+import Navbar from "./Components/Navbar";
 import Home from "./Components/Home";
 import Skills from "./Components/Skills";
 import About from "./Components/About";
 import Contacts from "./Components/Contacts";
-import Navbar from "./Components/Navbar";
-
-
 
 function App() {
-
   return (
     <>
       <Navbar />
-
       <Routes>
-      <Route path="/" element={<Home />}></Route>
+        <Route path="/" element={<Home />}></Route>
         <Route path="skills" element={<Skills />}></Route>
         <Route path="about" element={<About />}></Route>
         <Route path="contacts" element={<Contacts />}></Route>
