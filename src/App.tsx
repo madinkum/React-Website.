4a188e320988d10330fa89d751ecf3be178e8f08
Madinku <MadinkuFinny@gmail.com>
2023-10-24 08:22:57 +0200

    removing the import useLocation

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import useLocation from "react-router-dom"
 import Navbar from "./Components/Navbar";
 import Home from "./Components/Home";
 import Skills from "./Components/Skills";
